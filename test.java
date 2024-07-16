b58f8c7392e83331b7508c06d39a2686c689ef09
ovvosh <shiy6677@gmail.com>
2024-07-16 17:55:02 +0800

    test1

---
+++ test.java
@@ -0,0 +1,13 @@
+/**
+ * @BelongsProject: idea_git_test
+ * @BelongsPackage: PACKAGE_NAME
+ * @Author: ovvosh
+ * @CreateTime: 2024-07-16  17:53
+ * @Description: TODO
+ * @Version: 1.0
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("xxxx");
+    }
+}
