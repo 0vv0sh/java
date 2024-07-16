2562bd1c544eb99295c3a088dd13b8c42f3781bc
ovvosh <shiy6677@gmail.com>
2024-07-16 17:28:55 +0800

    push_test

---
+++ src/hello.java
@@ -0,0 +1,13 @@
+/**
+ * @BelongsProject: git_idea_test_push
+ * @BelongsPackage: PACKAGE_NAME
+ * @Author: ovvosh
+ * @CreateTime: 2024-07-16  17:14
+ * @Description: TODO
+ * @Version: 1.0
+ */
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
